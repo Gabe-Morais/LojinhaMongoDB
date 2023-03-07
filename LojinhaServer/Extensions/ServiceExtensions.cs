b1d7d10484ea06b029a012aad979a2ef3eee0043
Gabe-Morais <gabe-morais@outlook.com>
2023-03-07 20:01:58 -0300

    First Commit

--- LojinhaServer/Extensions/ServiceExtensions.cs
+++ LojinhaServer/Extensions/ServiceExtensions.cs
@@ -5,10 +5,34 @@ using System.Threading.Tasks;
 using LojinhaServer.Models;
 using MongoDB.Driver;
 
-namespace LojinhaServer.Extensions
-{
-    public class ServiceExtensions
+namespace LojinhaServer.Extensions;
+
+    public static class ServiceExtensions
     {
-        
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options=>
+            {
+                options.AddPolicy("CorsPolicy",
+                Builder => Builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureMongoDBSettings(this IServiceCollection services,
+        IConfiguration config)
+        {
+            services.Configure<MongoDBSettings>(
+                config.GetSection("MongoDBSettings")
+            );
+
+            services.AddSingleton<IMongoDatabase>(options =>{
+                var settings =
+                config.GetSection("MongoDBSettings").Get<MongoDBSettings>();
+                var client = new MongoClient(settings.ConnectionString);
+                return client.GetDatabase(settings.DatabaseName);
+            });
+        }
+
     }
-}
