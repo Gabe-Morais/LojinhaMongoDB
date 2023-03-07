dac7a292f334be83b694085afecee0de43bf13a5
Gabe-Morais <gabe-morais@outlook.com>
2023-03-07 19:52:01 -0300

    Inicio do projeto

---
+++ LojinhaServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LojinhaServer.Models;
+using MongoDB.Driver;
+
+namespace LojinhaServer.Extensions
+{
+    public class ServiceExtensions
+    {
+        
+    }
+}
